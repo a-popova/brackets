6eb67820d800738681174740ebaba94909f29994
a-popova <papova.anastasiya.13@gmail.com>
2019-10-02 12:40:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [str[0]];
+  
+    for (let i = 1; i < str.length; i++){
+      let a = stack[stack.length - 1];
+      let b = str[i];
+      let c = stack.length;
+      for (let j = 0; j < bracketsConfig.length; j++){ 
+        if (a == bracketsConfig[j][0] && b == bracketsConfig[j][1]){
+          stack.pop();
+        } 
+      }
+      if (c === stack.length){
+          stack.push(b);
+      }   
+    }
+  return stack.length == 0;
 }
+
